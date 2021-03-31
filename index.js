db4b8ca44664f5a955fd8a224286c98a0eab6191
Jennifer Pazos <jmp521@gmail.com>
2021-03-31 11:26:58 -0500

    commit tree f89c372da25cbb0f2bd8682008cbfb8a10a089c3

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let mainMain = document.querySelector("main#main");
+mainMain.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Your name is the champion";
